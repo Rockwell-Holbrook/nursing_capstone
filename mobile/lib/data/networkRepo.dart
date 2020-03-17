bd89f45469ee1a0ff8f87efb00e3b081be14fc14
tcstewart95 <tcstewart95@gmail.com>
2020-03-17 15:36:31 -0600

    Updating with comments and tyeps

--- mobile/lib/data/networkRepo.dart
+++ mobile/lib/data/networkRepo.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:dio/dio.dart' as dio;
 
-new_patient(user) async //creates a new patient and return there UUID
+//creates a new patient and return there UUID
+//Use when starting to record five audio files
+Future<String> new_patient(String user) async
 {
   var url = "https://api.byu-dept-nursingsteth-dev.amazon.byu.edu/beats/patients";
 
@@ -11,12 +13,13 @@ new_patient(user) async //creates a new patient and return there UUID
 
   final response_body = json.decode(request.body);
 
-  var id = response_body["id"];
+  String id = response_body["id"];
 
   return id;
 }
 
-upload_file(user, patient_id) async //user is an email
+//Writes one file for a patient to the db under the user's credentials
+void upload_file(String user, String patient_id, File file) async //user is an email
 {
   var url = "https://api.byu-dept-nursingsteth-dev.amazon.byu.edu/beats/patients/"+patient_id+"/recordings";
 
@@ -26,11 +29,14 @@ upload_file(user, patient_id) async //user is an email
 
   final response_body = json.decode(file_url.body);
 
-  var request = await put(response_body["url"], body: File('audio1.wav').readAsBytesSync());
+  var request = await put(response_body["url"], body: file.readAsBytesSync());
 
 }
 
-all_patients(token) async //return next token and all patients
+//For paginating on the admin page. If token is an empty String, sends first search back
+//This will return a token for requesting the next batch of users
+//To request the next batch of users, run the query again but use the token we had saved
+Future<dynamic> all_patients(String token) async //return next token and all patients
 {
   var url = "https://api.byu-dept-nursingsteth-dev.amazon.byu.edu/beats/patients/"+token;
 
@@ -42,7 +48,8 @@ all_patients(token) async //return next token and all patients
 
 }
 
-patient_recordings(patient_id) async //returns patients recordings as a list
+//Returns a list of URLs that are used in get_recordings to ask for the specific wav file
+Future<List<String>> patient_recordings(String patient_id) async //returns patients recordings as a list
 {
   var url = "https://api.byu-dept-nursingsteth-dev.amazon.byu.edu/beats/patients/"+patient_id+"/recordings";
 
@@ -50,7 +57,7 @@ patient_recordings(patient_id) async //returns patients recordings as a list
 
   final response_body = json.decode(request.body);
 
-  List<List> recordings = [];
+  List<String> recordings = [];
   for (int i  = 0; i < response_body["recordings"].length; i++)
   {
     final url = response_body["recordings"].elementAt(i);
@@ -63,8 +70,8 @@ patient_recordings(patient_id) async //returns patients recordings as a list
     return recordings;
 }
 
-
-get_recording(r_url) async //get one recording from url
+//Use one of the URLs queried from patient_recordings to get the WAV file
+Future<dynamic> get_recording(String r_url) async //get one recording from url
 {
   dio.Response<List<int>> rs = await dio.Dio().get<List<int>>(r_url,
   options: dio.Options(responseType: dio.ResponseType.bytes),
@@ -72,7 +79,8 @@ get_recording(r_url) async //get one recording from url
   return rs.data;
 }
 
-filter_list(filter) async //filter all patients
+///Pass a string of a filter
+Future<dynamic> filter_list(String filter) async //filter all patients
 {
   var url = "https://api.byu-dept-nursingsteth-dev.amazon.byu.edu/beats/patients"+filter;
 
@@ -84,68 +92,10 @@ filter_list(filter) async //filter all patients
 }
 
 //body format { "patient": { "tags": ["tag1", "tag2"], "abnormal": true}};
-update(patient_id, body) async 
+//Used when the administrator diagnoses and adds tags
+void update(String patient_id, body) async 
 {
   var url = "https://api.byu-dept-nursingsteth-dev.amazon.byu.edu/beats/patients/"+patient_id;
 
   var request = await put(url, body: JsonEncoder().convert(body));
 }
-
-// import 'dart:io';
-// import 'dart:convert';
-
-// Future<dynamic> executeQuery(var url, Map map) async {
-//   try{
-//     HttpClient httpClient = new HttpClient();
-//     HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
-//     request.headers.set('content-type', 'application/json');
-//     request.write(json.encode(map));
-//     HttpClientResponse response = await request.close();
-//     var reply = await response.cast<List<int>>().transform(utf8.decoder).join();
-//     print(reply);
-//     httpClient.close();
-//     return reply;
-//   } catch(e) {
-//     print(e);
-//   }
-// }
-
-// createPatient() async {
-//   var url = 'https://89z1b0o7y3.execute-api.us-west-2.amazonaws.com/dev/patients';
-//   Map map = {};
-//   var response;
-//   try {
-//     response = await executeQuery(url, map);
-//   } catch(e) {
-//     print(e);
-//   }
-//   return response;
-// }
-
-// Future<List<dynamic>> getPatients() async {
-//   return Future(() {
-//     List<dynamic> items =
-//       [
-//         {
-//           "id": "1111",
-//           "date": "01-01-1111",
-//           "recorder": "JoeAnn Meyers",
-//           "abnormal": "true"
-//         },
-//         {
-//           "id": "2222",
-//           "date": "02-02-2222",
-//           "recorder": "JoeAnn Meyers",
-//           "abnormal": "false"
-//         },
-//         {
-//           "id": "3333",
-//           "date": "03-03-3333",
-//           "recorder": "Billy Joe",
-//           "abnormal": "true"
-//         }
-//       ];
-
-//     return items;
-//     });
-//   }
