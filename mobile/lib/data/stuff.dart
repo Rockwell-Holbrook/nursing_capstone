7137c6d49ee16e43e18c0b7d01c55d3a695fff4e
tcstewart95 <tcstewart95@gmail.com>
2020-04-15 16:13:57 -0600

    prep to full from branches

---
+++ mobile/lib/data/stuff.dart
@@ -0,0 +1,4 @@
+  String userPoolID = 'us-west-2_jHUyTi4jF';
+  String adminPoolID = 'us-west-2_SrOMVtqVD';
+  String appClientID = 'iicpdglhedrsh8fr5bqp0sb73';
+  String adminAppClientID = '3acurksh5jm4hum2vuptnagq9d';
